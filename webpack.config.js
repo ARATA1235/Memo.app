70d8dc30b578302de9fd8f33a9cb1eaf198d5ca0
Arata <a.sasaki.sys23@morijyobi.ac.jp>
2024-05-29 11:39:36 +0900

    Add: 初回コミット

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { Extension } = require("typescript");
+
+module.exports = {
+    // ビルドした際の出力先
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "bundle.js"
+    },
+    // モード指定
+    mode: "development",
+    // import時のパス指定で拡張子を省略する
+    resolve: {
+        extensions: ['.ts','.js']
+    },
+    // 開発用サーバーで起動するバス
+    devServer: {
+        static: {
+            directory: `${__dirname}/dist`,
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+            },
+        ],
+    },
+}
